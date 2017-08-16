ac2ef3a74b23827d671833105b416d33ae7fef36
mari <maricrisbonzo@gmail.com>
2017-08-15 19:32:50 -0700

    Lesson 2, pt. 1: Removed unnecessary file

--- fs.js
+++
@@ -1,9 +0,0 @@
-// A function that will read the names from the directory and print them all out.
-
-const fs = require('fs');
-
-const printName = (err, data) => {
-  console.log(data);
-}
-
-fs.readdir('/home', printName);
