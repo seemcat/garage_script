daff0afbc364852eba7b7901594d075aaa889086
mari <maricrisbonzo@gmail.com>
2017-08-17 20:06:45 -0700

    Lesson 5: Delete form & send data via XMLHttpRequest

--- publicHTML.js
+++ publicHTML.js
@@ -7,5 +7,4 @@ app.use(express.static('public'));
 
 app.get('/submit', (req, res) => {
   fs.appendFile('/home/mc/garage_script/public/publicHTML.txt', `Name: ${req.query.name} Comment: ${req.query.comment} \n`);
-  res.send('Your comment has been received.');
 });
