d6123864faed33521828961e062424a80d30343b
mari <maricrisbonzo@gmail.com>
2017-09-20 12:40:19 -0700

    Ticket Challenge Completed!

--- public/app.js
+++ public/app.js
@@ -1,26 +1,38 @@
-const reducers = Redux.combineReducers({
-  toDolist: (state = { arrayOfErrands: [] }, action) => {
-    if(action.type === 'add'){
-      state.arrayOfErrands.push(action.errand);
+$('#buyMBTickets').click(() => {
+  const MBTicketsPurchased = $('#numOfMBTickets').val();
+  $.ajax({
+    type: 'POST',
+    url: '/buyTickets',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      MBTicketsPurchased
+    }),
+    success: (data) => {
+      console.log('Successful POST Request: ', data);
     }
-    return state;
-  }
+  });
 });
 
-const store = Redux.createStore(reducers);
-
-$('#submitErrand').click(() => {
-  const errand = $('#errand').val();
-  store.dispatch({
-    type: 'add',
-    errand
+$('#buySZTickets').click(() => {
+  const SZTicketsPurchased = $('#numOfSZTickets').val();
+  $.get(`http://sz.llip.life/buy?amnt=${SZTicketsPurchased}`, (data) => {
+    $('numOfSZTicketsLeft').html(data);
   });
-
-  const arrayOfErrandsToDisplay = store.getState().toDolist.arrayOfErrands;
-  let displayArrayOfErrands = '';
-  arrayOfErrandsToDisplay.forEach((e) => {
-    displayArrayOfErrands = displayArrayOfErrands + `<div>${e}</div>`;
-  });
-  $('#toDoList').html(displayArrayOfErrands);
 });
 
+const getNumOfSZTickets = () => {
+  $.get('http://sz.llip.life/tickets', (data) => {
+    $('#numOfSZTicketsLeft').html(data);
+  });
+}
+
+window.setInterval(getNumOfSZTickets, 500);
+
+/* Get chat file every second & display */
+const getNumOfMBTickets = () => {
+  $.get('/ticketsLeft', (data) => {
+    $('#numOfMBTicketsLeft').html(data);
+  });
+}
+
+window.setInterval(getNumOfMBTickets, 500);
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.listen(3000);
 app.use(express.static('public'));
+app.use(bodyParser.json({}));
 
-app.get('/', (req, res) => {
-  res.send('Made it to the website!');
+let numOfTickets = 100;
+
+app.post('/buyTickets', (req, res) => {
+  numOfTickets = numOfTickets - req.body.MBTicketsPurchased;
+  res.send(numOfTickets.toString());
+});
+
+app.get('/ticketsLeft', (req, res) => {
+  res.send(numOfTickets.toString());
 });
